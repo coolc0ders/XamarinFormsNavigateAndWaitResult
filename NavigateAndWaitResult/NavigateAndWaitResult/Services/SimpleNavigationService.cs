acdf82a939df7e7867ac81e2b909314a3e69baf2
Damien T.D.K <damientohin@gmail.com>
2020-01-14 22:56:57 +0100

    Completed Xamarin forms navigate and await

---
+++ NavigateAndWaitResult/NavigateAndWaitResult/Services/SimpleNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using NavigateAndWaitResult.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigateAndWaitResult.Services
+{
+    public class SimpleNavigationService
+    {
+        public async Task<T> NavigateToModal<T>(string modalName)
+        {
+            var source = new TaskCompletionSource<T>();
+            if (modalName == nameof(NewItemPage))
+            {
+                var page = new NewItemPage();
+                page.PageDisapearing += (result) =>
+                {
+                    var res = (T)Convert.ChangeType(result, typeof(T));
+                    source.SetResult(res);
+                };
+                await App.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(page));
+            }
+            return await source.Task;
+        }
+    }
+}
---
+++ NavigateAndWaitResult/NavigateAndWaitResult/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using NavigateAndWaitResult.Models;
+using NavigateAndWaitResult.Views;
+using NavigateAndWaitResult.Services;
+
+namespace NavigateAndWaitResult.ViewModels
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+        private SimpleNavigationService _navigationService;
+        public Command AddItemCommand { get; private set; }
+
+        public ItemsViewModel()
+        {
+            Title = "Browse";
+            Items = new ObservableCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            _navigationService = new SimpleNavigationService();
+            AddItemCommand = new Command(async () => await ExecuteAddItemCommand());
+        }
+
+        async Task ExecuteAddItemCommand()
+        {
+            var item = await new SimpleNavigationService().NavigateToModal<Item>(nameof(NewItemPage));
+            Items.Add(item);
+            await DataStore.AddItemAsync(item);
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
